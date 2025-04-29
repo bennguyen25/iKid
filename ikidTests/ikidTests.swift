3e9f1fd657fe7ff80a62e9d95e3480b0e8ec0042
akben <akben@uw.edu>
2025-04-29 13:35:00 -0700

    Initial Commit

---
+++ ikidTests/ikidTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikidTests.swift
+//  ikidTests
+//
+//  Created by Ben Nguyen on 4/29/25.
+//
+
+import Testing
+@testable import ikid
+
+struct ikidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
